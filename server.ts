7a4cb5677e2b435be7a4c735a908735f16d64d0d
karthikraja <pmnkarthikraja@gmail.com>
2024-07-29 17:22:23 +0530

    move db and app.ts functions into server.ts

--- server.ts
+++ server.ts
@@ -4,6 +4,7 @@ import cors from 'cors';
 import express from 'express';
 import clientRoutes from './routes/clientRoutes';
 require('dotenv').config();
+import mongoose from 'mongoose';
 
 export const app = express()
 app.use(bodyParser.urlencoded({ extended: false }))
@@ -23,6 +24,33 @@ export const startServer = () => {
     serverStarted=true
   });
 };
+
+const connectDBwithRetry = async (): Promise<void> => {
+  try {
+    await mongoose.connect(process.env.MONGO_URI as string);
+    console.log('MongoDB connected successfully');
+    if (!serverStarted) {
+      startServer();
+    }
+  } catch (error) {
+    console.error('Error connecting to MongoDB:', error);
+    setTimeout(connectDBwithRetry, 5000); // Retry connection after 5 seconds
+  }
+};
+
+// Handling mongoose connection error
+mongoose.connection.on('error', (err) => {
+  console.error('MongoDB connection error:', err);
+});
+
+// Try to connect with db once it gets disconnected
+mongoose.connection.on('disconnected', () => {
+  console.log('Lost MongoDB connection. Attempting to reconnect...');
+  connectDBwithRetry();
+});
+
+// Start the initial connection
+connectDBwithRetry();
 
 export const checkIsServerStarted = ():boolean =>{
   return serverStarted
