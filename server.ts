e5bc583fa8be3f26bef45276ac3d216a58984c5c
karthikraja <pmnkarthikraja@gmail.com>
2024-07-29 16:46:19 +0530

    remove /api

--- server.ts
+++ server.ts
@@ -14,7 +14,7 @@ app.use(cors({
 }))
 app.use(cookieParser())
 app.use(express.json())
-app.use('/api', clientRoutes);
+app.use('/', clientRoutes);
 
 let serverStarted=false
 export const startServer = () => {
